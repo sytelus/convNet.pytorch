b7f7d831c76a141f59b2b52e2198b201a395f6a6
Shital Shah <shital@ShitalShah.com>
2018-05-11 21:13:11 -0700

    phase 1

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import argparse
 import os
 import time
 import logging
+import warnings
+from datetime import datetime
+from ast import literal_eval
+
 import torch
 import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torch.optim
 import torch.utils.data
+
 import models
 from data import get_dataset
 from preprocess import get_transform
@@ -15,74 +20,82 @@ from utils.log import setup_logging, ResultsLog, save_checkpoint
 from utils.meters import AverageMeter, accuracy
 from utils.optim import OptimRegime
 from utils.misc import torch_dtypes
-from datetime import datetime
-from ast import literal_eval
 
-model_names = sorted(name for name in models.__dict__
-                     if name.islower() and not name.startswith("__")
-                     and callable(models.__dict__[name]))
 
-parser = argparse.ArgumentParser(description='PyTorch ConvNet Training')
+def getArgs(model_names):
+    parser = argparse.ArgumentParser(description='PyTorch ConvNet Training')
 
-parser.add_argument('--results_dir', metavar='RESULTS_DIR', default='./results',
-                    help='results dir')
-parser.add_argument('--save', metavar='SAVE', default='',
-                    help='saved folder')
-parser.add_argument('--dataset', metavar='DATASET', default='imagenet',
-                    help='dataset name or folder')
-parser.add_argument('--model', '-a', metavar='MODEL', default='alexnet',
-                    choices=model_names,
-                    help='model architecture: ' +
-                    ' | '.join(model_names) +
-                    ' (default: alexnet)')
-parser.add_argument('--input_size', type=int, default=None,
-                    help='image input size')
-parser.add_argument('--model_config', default='',
-                    help='additional architecture configuration')
-parser.add_argument('--dtype', default='float',
-                    help='type of tensor: ' +
-                    ' | '.join(torch_dtypes.keys()) +
-                    ' (default: half)')
-parser.add_argument('--device', default='cuda',
-                    help='device assignment ("cpu" or "cuda")')
-parser.add_argument('--device_ids', default=[0], type=int, nargs='+',
-                    help='device ids assignment (e.g 0 1 2 3')
-parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
-                    help='number of data loading workers (default: 8)')
-parser.add_argument('--epochs', default=90, type=int, metavar='N',
-                    help='number of total epochs to run')
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
-                    help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=256, type=int,
-                    metavar='N', help='mini-batch size (default: 256)')
-parser.add_argument('--optimizer', default='SGD', type=str, metavar='OPT',
-                    help='optimizer function used')
-parser.add_argument('--lr', '--learning_rate', default=0.1, type=float,
-                    metavar='LR', help='initial learning rate')
-parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                    help='momentum')
-parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
-                    metavar='W', help='weight decay (default: 1e-4)')
-parser.add_argument('--print-freq', '-p', default=10, type=int,
-                    metavar='N', help='print frequency (default: 10)')
-parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
-parser.add_argument('-e', '--evaluate', type=str, metavar='FILE',
-                    help='evaluate model FILE on validation set')
-parser.add_argument('--seed', default=123, type=int,
-                    help='random seed (default: 123)')
+    parser.add_argument('--results_dir', metavar='RESULTS_DIR', default='./results',
+                        help='results dir')
+    parser.add_argument('--save', metavar='SAVE', default='',
+                        help='saved folder')
+    parser.add_argument('--dataset', metavar='DATASET', default='imagenet',
+                        help='dataset name or folder')
+    parser.add_argument('--model', '-a', metavar='MODEL', default='alexnet',
+                        choices=model_names,
+                        help='model architecture: ' +
+                        ' | '.join(model_names) +
+                        ' (default: alexnet)')
+    parser.add_argument('--input_size', type=int, default=None,
+                        help='image input size')
+    parser.add_argument('--model_config', default='',
+                        help='additional architecture configuration')
+    parser.add_argument('--dtype', default='float',
+                        help='type of tensor: ' +
+                        ' | '.join(torch_dtypes.keys()) +
+                        ' (default: half)')
+    parser.add_argument('--device', default='cuda',
+                        help='device assignment ("cpu" or "cuda")')
+    parser.add_argument('--device_ids', default=[0], type=int, nargs='+',
+                        help='device ids assignment (e.g 0 1 2 3')
+    parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
+                        help='number of data loading workers (default: 8)')
+    parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                        help='number of total epochs to run')
+    parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                        help='manual epoch number (useful on restarts)')
+    parser.add_argument('-b', '--batch-size', default=256, type=int,
+                        metavar='N', help='mini-batch size (default: 256)')
+    parser.add_argument('--optimizer', default='SGD', type=str, metavar='OPT',
+                        help='optimizer function used')
+    parser.add_argument('--lr', '--learning_rate', default=0.1, type=float,
+                        metavar='LR', help='initial learning rate')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)')
+    parser.add_argument('--print-freq', '-p', default=10, type=int,
+                        metavar='N', help='print frequency (default: 10)')
+    parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                        help='path to latest checkpoint (default: none)')
+    parser.add_argument('-e', '--evaluate', type=str, metavar='FILE',
+                        help='evaluate model FILE on validation set')
+    parser.add_argument('--seed', default=123, type=int,
+                        help='random seed (default: 123)')
+
+    return parser.parse_args()
 
 
 def main():
     global args, best_prec1, dtype
-    best_prec1 = 0
-    args = parser.parse_args()
-    dtype = torch_dtypes.get(args.dtype)
+
+    #get available models
+    model_names = sorted(name for name in models.__dict__
+                        if name.islower() and not name.startswith("__")
+                        and callable(models.__dict__[name]))
+
+    #parse command line
+    args = getArgs(model_names)
+    logging.debug("run arguments: %s", args)
+
+    #set seed    
     torch.manual_seed(args.seed)
-    time_stamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+    #setup logging
     if args.evaluate:
         args.results_dir = '/tmp'
     if args.save is '':
+        time_stamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
         args.save = time_stamp
     save_path = os.path.join(args.results_dir, args.save)
     if not os.path.exists(save_path):
@@ -95,14 +108,18 @@ def main():
         results_path, title='Training Results - %s' % args.save)
 
     logging.info("saving to %s", save_path)
-    logging.debug("run arguments: %s", args)
 
-    if 'cuda' in args.device and torch.cuda.is_available():
-        torch.cuda.manual_seed_all(args.seed)
-        torch.cuda.set_device(args.device_ids[0])
-        cudnn.benchmark = True
-    else:
-        args.device_ids = None
+    #setup device and dtype
+    multi_device = False
+    dtype = torch_dtypes.get(args.dtype)
+    if 'cuda' in args.device:
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(args.seed)
+            torch.cuda.set_device(args.device_ids[0])
+            cudnn.benchmark = True
+            multi_device = len(args.device_ids) > 1
+        else:
+            logging.warning("CUDA was requested but not available.")
 
     # create model
     logging.info("creating model %s", args.model)
@@ -116,6 +133,7 @@ def main():
     logging.info("created model with configuration: %s", model_config)
 
     # optionally resume from a checkpoint
+    best_prec1 = 0 #last best result
     if args.evaluate:
         if not os.path.isfile(args.evaluate):
             parser.error('invalid checkpoint: {}'.format(args.evaluate))
@@ -143,7 +161,7 @@ def main():
     num_parameters = sum([l.nelement() for l in model.parameters()])
     logging.info("number of parameters: %d", num_parameters)
 
-    # Data loading code
+    # data loading code
     default_transform = {
         'train': get_transform(args.dataset,
                                input_size=args.input_size, augment=True),
@@ -151,36 +169,46 @@ def main():
                               input_size=args.input_size, augment=False)
     }
     transform = getattr(model, 'input_transform', default_transform)
-    regime = getattr(model, 'regime', [{'epoch': 0,
-                                        'optimizer': args.optimizer,
-                                        'lr': args.lr,
-                                        'momentum': args.momentum,
-                                        'weight_decay': args.weight_decay}])
 
-    # define loss function (criterion) and optimizer
+    # define loss function (criterion)
     criterion = getattr(model, 'criterion', nn.CrossEntropyLoss)()
+
+    # move things to device
     criterion.to(args.device, dtype)
     model.to(args.device, dtype)
 
+    # validation dataset
     val_data = get_dataset(args.dataset, 'val', transform['eval'])
     val_loader = torch.utils.data.DataLoader(
         val_data,
         batch_size=args.batch_size, shuffle=False,
         num_workers=args.workers, pin_memory=True)
 
+    # validation mode
     if args.evaluate:
         validate(val_loader, model, criterion, 0)
         return
 
-    train_data = get_dataset(args.dataset, 'train', transform['train'])
+    # train mode
+
+    # define optimizer
+    regime = getattr(model, 'regime', [{'epoch': 0,
+                                        'optimizer': args.optimizer,
+                                        'lr': args.lr,
+                                        'momentum': args.momentum,
+                                        'weight_decay': args.weight_decay}])
+
+    optimizer = OptimRegime(model.parameters(), regime)
+    logging.info('training regime: %s', regime)
+
+    # training data
+    train_data = get_dataset(args.dataset, 'train', transform['train'])    
     train_loader = torch.utils.data.DataLoader(
         train_data,
         batch_size=args.batch_size, shuffle=True,
         num_workers=args.workers, pin_memory=True)
 
-    optimizer = OptimRegime(model.parameters(), regime)
-    logging.info('training regime: %s', regime)
-
+    # start training
     for epoch in range(args.start_epoch, args.epochs):
         # train for one epoch
         train_loss, train_prec1, train_prec5 = train(
@@ -212,6 +240,7 @@ def main():
                              train_prec1=train_prec1, val_prec1=val_prec1,
                              train_prec5=train_prec5, val_prec5=val_prec5))
 
+        # update result log
         results.add(epoch=epoch + 1, train_loss=train_loss, val_loss=val_loss,
                     train_error1=100 - train_prec1, val_error1=100 - val_prec1,
                     train_error5=100 - train_prec5, val_error5=100 - val_prec5)
@@ -226,32 +255,33 @@ def main():
                      title='Error@5', ylabel='error %')
         results.save()
 
-
+# forward pass used by train() and validate()
 def forward(data_loader, model, criterion, epoch=0, training=True, optimizer=None):
-    if args.device_ids and len(args.device_ids) > 1:
+    if multi_device:
         model = torch.nn.DataParallel(model, args.device_ids)
-        
+    
+    # init perf counters
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
     top5 = AverageMeter()
 
-    end = time.time()
-    for i, (inputs, target) in enumerate(data_loader):
+    batch_end_time = time.time()
+    for i, (inputs, label) in enumerate(data_loader):
         # measure data loading time
-        data_time.update(time.time() - end)
-        target = target.to(args.device)
+        data_time.update(time.time() - batch_end_time)
+        label = label.to(args.device)
         inputs = inputs.to(args.device, dtype=dtype)
 
         # compute output
         output = model(inputs)
-        loss = criterion(output, target)
-        if type(output) is list:
+        loss = criterion(output, label)
+        if type(output) is list: # might be the case if we have single scaler
             output = output[0]
 
         # measure accuracy and record loss
-        prec1, prec5 = accuracy(output.detach(), target, topk=(1, 5))
+        prec1, prec5 = accuracy(output.detach(), label, topk=(1, 5))
         losses.update(float(loss), inputs.size(0))
         top1.update(float(prec1), inputs.size(0))
         top5.update(float(prec5), inputs.size(0))
@@ -264,8 +294,8 @@ def forward(data_loader, model, criterion, epoch=0, training=True, optimizer=Non
             optimizer.step()
 
         # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
+        batch_time.update(time.time() - batch_end_time)
+        batch_end_time = time.time()
 
         if i % args.print_freq == 0:
             logging.info('{phase} - Epoch: [{0}][{1}/{2}]\t'
